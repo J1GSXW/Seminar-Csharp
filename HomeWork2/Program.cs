c0d05fcacc8585b551d3f807de7d4f17988c956d
Igor Andrushenko <igorandrushenko2001@gmail.com>
2023-03-29 14:36:40 +1000

    Do a HomeWork2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,61 @@
+﻿//Task 1.
+//Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// int SecondNum(int number)
+// {
+//     int num1 = number / 10;
+//     int num2 = num1 % 10;
+//     return num2;
+// }
+// Console.Write("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(SecondNum(num));
+
+
+//Task 2. Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+// int ThirdNum(int number)
+// {
+//     if(number < 100)
+//     {
+//         return number;
+//     }
+//     else
+//     {
+//     while (number > 1000)
+//     {
+//         number = number / 10 ;
+//     }
+//     return number % 10;
+//     }
+// }
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number == ThirdNum(number))
+// {
+//     Console.WriteLine($"В числе {number} отсутствует третья цифра");
+// }
+// else
+// {
+//     Console.WriteLine(ThirdNum(number));
+// }
+
+//Task 3. Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// bool WeekendFind(int date)
+// {
+//     if (date >= 1 && date <= 5 || date > 8)
+//     {
+//         return false;
+//     }
+//     else
+//     {
+//         return true;
+//     }
+// }
+
+// Console.Write("Введите число: ");
+// int weekDay = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(WeekendFind(weekDay));
---
+++ Семинар 2/Program.cs
@@ -0,0 +1,88 @@
+﻿//Task 1.
+//Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
+
+// int CutNumber(int num)
+// {
+//     int hundreds = num / 100;
+//     int units = num % 10;
+
+//     int result = hundreds * 10 + units;
+//     return result;
+// }
+
+
+// int randNumber = new Random().Next(100, 1000);
+// int newNumber = CutNumber(randNumber);
+
+// Console.WriteLine($"New version of {randNumber} is {newNumber}");
+
+//Task 2.
+// Напишите программу, которая выводит
+//  случайное число из отрезка [10, 99]
+//  и показывает наибольшую цифру числа.
+// int MaxNumber(int max)
+// {
+//     int num1 = max / 10;
+//     int num2 = max % 10;
+//     if (num1 > num2)
+//     {
+//         return num1;
+//     }
+//     else
+//     {
+//         return num2;
+//     }
+// }
+
+// int randNumber = new Random().Next(10, 100);
+// int newNumber = MaxNumber(randNumber);
+// Console.WriteLine($"Максимальное цифра из {randNumber} - это {newNumber}");
+
+
+
+
+// Task 3.
+// Напишите программу, которая принимает на вход число и проверяет,
+//  кратно ли оно одновременно a и b.
+// bool QuadFind(int number, int div1, int div2)
+// {
+//     if(number % div1 == 0 && number % div2 == 0)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+// Console.WriteLine("Введите число для проверки: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число A : ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B : ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+
+// bool result = QuadFind(num1, numA, numB);
+// Console.WriteLine(result);
+
+
+//Task 4.
+//Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+// bool QuadFind(int num1, int num2)
+// {
+//     if (num1 == num2 * num2 || num2 == num1 * num1)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+// Console.WriteLine("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(QuadFind(num1, num2));
