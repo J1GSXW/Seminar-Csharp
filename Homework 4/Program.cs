a8f74e47262f5af11a8f7465cd4d206b86cfc9cd
Igor Andrushenko <igorandrushenko2001@gmail.com>
2023-04-13 10:41:05 +1000

    do a homework 4

---
+++ Homework 4/Program.cs
@@ -0,0 +1,69 @@
+﻿//Task 1.
+//Напишите цикл, который принимает на вход два числа (A и B)
+//и возводит число A в натуральную степень B.
+// int SquareFind (int num1, int num2)
+// {
+//     int step = num1;
+//     for(int count = 1;count < num2;count++)
+//     step = step * num1;
+//     return step;
+// }
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int result = SquareFind(a,b);
+// Console.Write($"Число {a} в степени {b} равно {result}");
+
+//Task 2.
+//Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+// int SummFind(int num)
+// {
+//     int sum = 0;
+//     while (num > 0)
+//     {
+//         int i = num % 10;
+//         num = num / 10;
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int result = SummFind(number);
+// Console.WriteLine($"Сумма всех цифр в числе {number} равна {result}");
+
+//Task 3.
+//Напишите программу, 
+//которая задаёт массив из 8 элементов
+// и выводит их на экран.
+// int[] CreateArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+
+//     for(int i = 0; i < size; i++)
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//         return array;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i<array.Length; i++)
+//         Console.Write(array[i] + " ");
+//     Console.WriteLine();
+// }
+
+// Console.Write("Задайте размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите минимальное значение: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальное значение: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] newArray = CreateArray(size, min, max);
+// ShowArray(newArray);
--- Семинар 4/Program.cs
+++ Семинар 4/Program.cs
@@ -48,29 +48,29 @@
 // Console.WriteLine($"Факториал числа {num} = {MultNum(num)}");
 
 
-// int[] CreateRandomArray(int size, int minValue, int maxValue)
-// {
-//     int[] array = new int[size];
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
     
-//     for(int i = 0; i < size; i++)
-//         array[i] = new Random().Next(minValue, maxValue + 1);
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
 
-//     return array;
-// }
+    return array;
+}
 
-// void ShowArray(int[] array)
-// {
-//     for(int i = 0; i < array.Length; i++)
-//         Console.Write(array[i] + " ");
-//     Console.WriteLine();
-// }
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
 
-// Console.Write("Input a size for array: ");
-// int size = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input a min possible value: ");
-// int min = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input a max possible value: ");
-// int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a size for array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
 
-// int[] newArray = CreateRandomArray(size, min, max);
-// ShowArray(newArray);
+int[] newArray = CreateRandomArray(size, min, max);
+ShowArray(newArray);
